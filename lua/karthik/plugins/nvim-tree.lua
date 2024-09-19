1f9e3fdf353ed9e4e8ce5fbb2a2d9f37209743e6
KArtHiK <karthiknatarajan04@gmail.com>
2024-09-19 08:48:02 +0200

    nvim tree to right side

--- lua/karthik/plugins/nvim-tree.lua
+++ lua/karthik/plugins/nvim-tree.lua
@@ -1,7 +1,11 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	config = function()
-		require("nvim-tree").setup()
+		require("nvim-tree").setup({
+			view = {
+				side = "right",
+			},
+		})
 		vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
 	end,
 }
